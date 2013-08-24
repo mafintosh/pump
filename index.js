b49d103912b72b2c52bef64e084ac1c2f77a6c91
Mathias Buus <mathiasbuus@gmail.com>
2013-08-24 16:56:26 +0200

    two streams required

--- index.js
+++ index.js
@@ -62,11 +62,11 @@ var pump = function() {
 	var callback = functionish(streams[streams.length-1]) && streams.pop() || noop;
 
 	if (Array.isArray(streams[0])) streams = streams[0];
+	if (streams.length < 2) throw new Error('pump requires two streams per minimum');
 
 	var error;
-	var readables = Math.max(streams.length-1, 1);
 	var destroys = streams.map(function(stream, i) {
-		var reading = i < readables;
+		var reading = i < streams.length-1;
 		var writing = i > 0;
 		return destroyer(stream, reading, writing, function(err) {
 			if (!error) error = err;
