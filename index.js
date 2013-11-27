bed0cd414127a00e354ba1bd8ebd35aece77bdf3
Mathias Buus <mathiasbuus@gmail.com>
2013-11-27 02:16:32 +0100

    using end-of-stream to detect stream close

--- index.js
+++ index.js
@@ -1,49 +1,44 @@
 var once = require('once');
+var eos = require('end-of-stream');
+var fs = require('fs'); // we only need fs to get the ReadStream and WriteStream prototypes
+
 var noop = function() {};
 
-var patch = function(stream, callback) { // patch 0.8 stream since they dont emit finish
-	var end = stream.end;
-	stream.end = function() {
-		callback();
-		end.apply(this, arguments);
-	};
+var isFn = function(fn) {
+	return typeof fn === 'function';
+};
+
+var isFS = function(stream) {
+	return (stream instanceof fs.ReadStream || stream instanceof fs.WriteStream) && isFn(stream.close);
+};
+
+var isRequest = function(stream) {
+	return stream.setHeader && typeof isFn(stream.abort);
 };
 
 var destroyer = function(stream, reading, writing, callback) {
 	callback = once(callback);
 
-	var destroyed = false;
 	var closed = false;
-
-	var onfinish = function() {
-		writing = false;
-		if (!reading) callback();
-	};
-
-	stream.on('error', callback);
-
-	stream.on('finish', onfinish);
-
-	stream.on('end', function() {
-		reading = false;
-		if (!writing) callback();
-	});
-
 	stream.on('close', function() {
 		closed = true;
-		if (!reading && !writing) return;
-		if (reading && stream._readableState && stream._readableState.ended) return;
-		callback(new Error('stream closed'));
 	});
 
-	if (writing && stream.writable && !stream._writableState) patch(stream, onfinish);
+	eos(stream, {readable:reading, writable:writing}, callback);
 
-	return function() {
-		if (closed || destroyed || (!reading && !writing)) return;
+	var destroyed = false;
+	return function(err) {
+		if (closed) return;
+		if (destroyed) return;
 		destroyed = true;
-		if (stream.destroy) return stream.destroy();
-		if (stream.end) return stream.end();
-		callback(new Error('stream was destroyed'));
+
+		if (isFS(stream)) return stream.close(); // use close for fs streams to avoid fd leaks
+		if (isRequest(stream)) return stream.abort(); // request.destroy just do .end - .abort is what we want
+
+		if (isFn(stream.destroy)) return stream.destroy();
+		if (isFn(stream.close)) return stream.close();
+
+		callback(err || new Error('stream was destroyed'));
 	};
 };
 
@@ -55,13 +50,9 @@ var pipe = function(from, to) {
 	return from.pipe(to);
 };
 
-var functionish = function(fn) {
-	return !fn || typeof fn === 'function';
-};
-
 var pump = function() {
 	var streams = Array.prototype.slice.call(arguments);
-	var callback = functionish(streams[streams.length-1]) && streams.pop() || noop;
+	var callback = isFn(streams[streams.length-1] || noop) && streams.pop() || noop;
 
 	if (Array.isArray(streams[0])) streams = streams[0];
 	if (streams.length < 2) throw new Error('pump requires two streams per minimum');
