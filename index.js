625f8acca2c89e685f879425a279d10819907d10
Ruben Bridgewater <ruben@bridgewater.de>
2018-02-17 16:33:31 +0100

    chore: improve callback check
    The first part of the expression is always thruthy and can therefore
    be removed.

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ var pipe = function (from, to) {
 
 var pump = function () {
   var streams = Array.prototype.slice.call(arguments)
-  var callback = isFn(streams[streams.length - 1] || noop) && streams.pop() || noop
+  var callback = streams.pop() || noop
 
   if (Array.isArray(streams[0])) streams = streams[0]
   if (streams.length < 2) throw new Error('pump requires two streams per minimum')
